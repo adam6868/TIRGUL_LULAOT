a62474b68119750bf05da4c4cbe99c7cb786e2d0
Adam Kaplanski <adamkaplanski6@gmail.com>
2021-03-15 19:56:12 +0200

    Add project files.

---
+++ TIRGUL_LULAOT/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIRGUL_LULAOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex 3
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    int num1, num2;// מספרים שלם
+            //    Console.WriteLine("enter 2 numbers");
+            //    num1 = int.Parse(Console.ReadLine());
+            //    num2 = int.Parse(Console.ReadLine());
+            //    if (num1 > 0 && num2 > 0 && num1 % 2 != 0 && num2 % 2 != 0)
+            //    {
+            //        Console.WriteLine(num1 + num2);
+
+            //    }
+            //            enter 2 numbers
+            //22
+            //54
+            //enter 2 numbers
+            //23
+            //53
+            //76
+
+
+
+            //ex4
+            //int num1, num2;
+            //Console.WriteLine("enter how many tasks");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < 30; i++)
+            //{
+            //    Console.WriteLine("enter how many tasks you did");
+            //    num2 = int.Parse(Console.ReadLine());
+            //    if (num2 == num1)
+            //    {
+            //        Console.WriteLine("you did all the work");
+            //    }
+            //    if (num1 > num2 && num2 > 0)
+            //    {
+            //        Console.WriteLine("you did some of the work");
+            //    }
+            //    if (num2 == 0)
+            //    {
+            //        Console.WriteLine("you didnt do any work");
+            //    }
+
+
+            //}
+            //enter how many tasks
+            //6
+            //enter how many tasks you did
+            //6
+            //you did all the work
+            //enter how many tasks you did
+
+            //ex5
+            int num1, num2, num3;
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+
+
+        }
+        }
+    }
+
+
